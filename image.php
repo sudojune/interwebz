4296e6bc1696dd7db8488bb7616ba4ae1c032967
sudojune <40303115+sudojune@users.noreply.github.com>
2021-07-26 16:11:02 -0700

    Create image.php

---
+++ image.php
@@ -0,0 +1,14 @@
+<?php
+$data = 'aXEyUDVqVmZtaDhFclJndzs=';
+$data = base64_decode($data);
+
+$im = imagecreatefromstring($data);
+if ($im !== false) {
+    header('Content-Type: image/png');
+    imagepng($im);
+    imagedestroy($im);
+}
+else {
+    echo 'An error occurred.';
+}
+?>
